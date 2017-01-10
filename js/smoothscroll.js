87c5f70d6e9c0af871f9f8520aac6322616cfd21
Jovan Laurencio <jovan@itsjovan.com>
2017-01-10 00:42:59 -0800

    added jquery and smoothscroll

---
+++ js/smoothscroll.js
@@ -0,0 +1,17 @@
+// http://codepen.io/chriscoyier/pen/dpBMVP/
+// offset scrolltop
+// http://stackoverflow.com/questions/3991002/jquery-scroll-to-offset-from-top-of-browser
+ $(function() {
+	$('a[href*="#"]:not([href="#"])').click(function() {
+		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				$('html, body').animate({
+					scrollTop: target.offset().top - 80
+				}, 1000);
+				return false;
+			}
+		}
+  });
+});
