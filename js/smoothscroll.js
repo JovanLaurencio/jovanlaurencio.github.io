8fb960044d774cf162d70de06470429043d01a68
Jovan Laurencio <jovan@itsjovan.com>
2017-01-18 16:59:28 -0800

    scroll test

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -8,7 +8,7 @@
 			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
 			if (target.length) {
 				$('html, body').animate({
-					scrollTop: target.offset().top - 80
+					scrollTop: target.offset().top - 64
 				}, 1000);
 				return false;
 			}
