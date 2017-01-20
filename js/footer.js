8cc7fddc8f31248d90bb2c460a283dda8027b627
Jovan Laurencio <jovan@itsjovan.com>
2017-01-20 11:55:45 -0800

    quotes added

--- js/footer.js
+++ js/footer.js
@@ -11,14 +11,14 @@ function peepsOnSite() {
  */
 
 var facts = [
-	"This is random cool fact #1.",
-	"This is random cool fact #2.",
-	"This is random cool fact #3.",
-	"This is random cool fact #4.",
-	"This is random cool fact #5.",
-	"This is random cool fact #6.",
-	"This is random cool fact #7.",
-	"This is random cool fact #8."
+	"Grit is passion and perseverance for very long-term goals... And working really hard to make that future a reality. - Angela Duckworth, GRIT",
+	"When you see only problems, you're not seeing clearly. - Phil Knight, SHOE DOG",
+	"Opportunity lies in the place where the complaints are. - Jack Ma, ALIBABA: THE HOUSE THAT JACK MA BUILT",
+	"Listen with empathy and overcommunicate with transparency. - Howard Schultz, ONWARD",
+	"Uncurious people do not lead examined lives, they cannot see causes that lie deeper than the surface - Yvon Chouinard, LET MY PEOPLE GO SURFING"
+//	"This is random cool fact #6.",
+//	"This is random cool fact #7.",
+//	"This is random cool fact #8."
 ]
 function oneCoolFact() {
   var myFact = facts[Math.floor(Math.random()*facts.length)]
