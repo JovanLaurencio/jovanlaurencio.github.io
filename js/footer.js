a2131a7854273bc74e87f1cb6049d92d0841eca3
Jovan Laurencio <jovan@itsjovan.com>
2017-01-21 12:47:46 -0800

    layout css addns

--- js/footer.js
+++ js/footer.js
@@ -11,11 +11,11 @@ function peepsOnSite() {
  */
 
 var facts = [
-	"Grit is passion and perseverance for very long-term goals... And working really hard to make that future a reality. - Angela Duckworth, GRIT",
-	"When you see only problems, you're not seeing clearly. - Phil Knight, SHOE DOG",
-	"Opportunity lies in the place where the complaints are. - Jack Ma, ALIBABA: THE HOUSE THAT JACK MA BUILT",
-	"Listen with empathy and overcommunicate with transparency. - Howard Schultz, ONWARD",
-	"Uncurious people do not lead examined lives, they cannot see causes that lie deeper than the surface - Yvon Chouinard, LET MY PEOPLE GO SURFING"
+	"Grit is passion and perseverance for very long-term goals... And working really hard to make that future a reality. - Angela Duckworth",
+	"When you see only problems, you're not seeing clearly. - Phil Knight",
+	"Opportunity lies in the place where the complaints are. - Jack Ma",
+	"Listen with empathy and overcommunicate with transparency. - Howard Schultz",
+	"Uncurious people do not lead examined lives, they cannot see causes that lie deeper than the surface - Yvon Chouinard"
 //	"This is random cool fact #6.",
 //	"This is random cool fact #7.",
 //	"This is random cool fact #8."
